1ff4f844fb0d8b215930fe5ba0c827c2e389782b
sonia246580 <s-A115-29@CAMPUS.UEK.KRAKOW.PL>
2025-11-16 12:42:09 +0100

    dodanie plikow 7.2-7.8

---
+++ 7.4.py
@@ -0,0 +1,4 @@
+diameter = int(input('Enter tree circumference: '))
+tree_to_cut_down = diameter <= 50
+
+print(f'Tree can be cut down: {tree_to_cut_down} ')
---
+++ 7.8.py
@@ -0,0 +1,10 @@
+###
+# A program that prints results of three dice rolls
+# and the sum of dice rolled.
+#
+import random
+dice_roll_1 = random.randint(...)
+dice_roll_2 = ...
+...
+total = dice_roll_1 + ...
+print(...)
